71f3aa9c3d7e096d8bfc9fb118154fb741fdd4a7
snx2 <lunguj665@gmail.com>
2025-11-10 14:11:35 +0200

    fixed the memory.py merge conflicts

--- utils/memory.py
+++ utils/memory.py
@@ -1,8 +1,9 @@
 
 import os
 
+
 def update_history(operations: str):
-    """Replaces operation words with symbols and saves them to a history file."""
+    """Replace operation word with symbols and saves them to a history file."""
     symbols = {
         "add": "+",
         "sub": "-",
@@ -27,13 +28,13 @@ def display_history():
             lines = file.readlines()
 
         if not lines:
-            print("History is empty.")
+            return "History is empty."
         else:
             for i, line in enumerate(lines, start=1):
-                print(f"{i} | {line.strip()}")
+                return f"{i} | {line.strip()}"
 
     except FileNotFoundError:
-        print("No history found.")
+        return "No history found."
 
 
 def delete_history():
@@ -41,37 +42,11 @@ def delete_history():
     try:
         os.remove("./utils/history")  # Remove the old history file
     except FileNotFoundError:
-        pass  # If it doesn’t exist, just ignore the error
+        return FileNotFoundError
+        # If it doesn’t exist, just ignore the error
 
     # Create a new empty file for future history
     with open("./utils/history", "x") as f:
         pass
 
-    print("History cleared.")
-
-
-
-
-
-
-
-
-
-# this is where th memory management code shoukd be
-history = []
-
-# function to add entry to history
-
-
-def add_to_history(calculation):
-    history.append(calculation)
-# function to display all history
-
-
-def show_history():
-    if not history:
-        print("No history yet.")
-    else:
-        print("Calculation History:")
-        for item in history:
-            print(ite
+    return "History cleared."
