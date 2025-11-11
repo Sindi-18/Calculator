9a9ffaf3b240d82a001e151e0cd0474bd7c89dbd
solochi776 <shadreckk222@gmail.com>
2025-11-11 12:27:05 +0200

    test verification of history

--- utils/test_operators.py
+++ utils/test_operators.py
@@ -6,10 +6,14 @@ def test_true():
     assert op.multiply(5, 5) == 25
     assert op.multiply("5", 5) == 25
     assert op.multiply("3.5", 2) == 7.0
-
-
+    assert op.divide(10,5) == 2
+    assert op.divide("10",2) == 5
 def test_false():
     with pytest.raises(ValueError):
         op.multiply("a", "b")
     with pytest.raises(TypeError):
         op.multiply([1, 2], 5)
+
+    with pytest.raises(ValueError):
+        op.divide("a","b")
+        def verify_history(history, expected_operations):
